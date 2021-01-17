b2efcb7c14e30741abb55e51779f3e4d414f28f4
Eddie Russell <eddierussell18@gmail.com>
2021-01-17 13:01:05 -0500

    Updated TimeSheet

--- TimeSheet/Script.js
+++ TimeSheet/Script.js
@@ -1,4 +1,4 @@
-var StartTime, EndTime, SubmitTime, Schedule, StartDate, EndDate, Reason, Day;
+var StartTime, EndTime, SubmitTime, Schedule, StartDate, EndDate, Reason, Day, CalcTime, Weekdays;
 //Declare the Variables
 StartTime = document.getElementById("StartTime");
 EndTime = document.getElementById("EndTime");
@@ -6,7 +6,10 @@ SubmitTime = document.getElementById("SubmitTime");
 Schedule = document.getElementById("Schedule");
 StartDate = document.getElementById("StartDate");
 Reason = document.getElementById("Reason");
-Day = document.getElementById("Day")
+//Day = document.getElementById("Day")
+CalcTime = document.getElementById("CalcTime");
+Weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+TotalH = 0;
 
 //When clicked will add inputs to the textarea
 SubmitTime.onclick = function() {
@@ -15,12 +18,20 @@ SubmitTime.onclick = function() {
     var StartMonth = StartDate.value.slice(5,7); //Gets the Month from date value
     var StartDay = StartDate.value.slice(8,10); //Gets the Day
     var StartYear = StartDate.value.slice(0,4); //Gets the Year
-
     var FullDate = StartMonth + "/" + StartDay + "/" + StartYear; //Formats the values
+    //Gets the Day from Date Input
+    var SDay = new Date(StartDate.value);
+    var Weekday = SDay.getDay();
+    console.log(Weekdays[Weekday]);
+
     // Puts the information into the textarea
-    Schedule.innerHTML += Day.value + ", " + FullDate + ", " + StartTime.value + " - " + EndTime.value + ": " + WorkedHours + " Hours" + "\n" + "Reason: " + Reason.value + "\n \n";
+    var PrintSch = Schedule.innerHTML += Weekdays[Weekday] + ", " + FullDate + ", " + StartTime.value + " - " + EndTime.value + ": " + WorkedHours + " Hours" + "\n" + "Reason: " + Reason.value + "\n \n";
+    // Gets Total Hours Worked
+    TotalH += WorkedHours;
+
+    CalcTime.onclick = function() {
+        Schedule.innerHTML = PrintSch +"\n \n" + TotalH + " Hours Worked";
+    }
 }
 
 
-
-// 4.5 Hours Worked
