2f4031974785e3f14bbf1531359d6c2c56c842dd
Eddie Russell <eddierussell18@gmail.com>
2021-01-27 14:28:32 -0500

    Updated TimeSheet

--- TimeSheet/Script.js
+++ TimeSheet/Script.js
@@ -10,10 +10,19 @@ Reason = document.getElementById("Reason"),
 CalcTime = document.getElementById("CalcTime"),
 Weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 let TotalH = 0;
-
+let WorkedHours;
 //When clicked will add inputs to the textarea
 SubmitTime.onclick = function() {
-    let WorkedHours = parseInt(EndTime.value.slice(0,2)) - parseInt(StartTime.value.slice(0,2)); //Adds the Start Time + End Time to get hours worked
+
+    if (EndTime.value.slice(4,8) =='PM'){// If the end time is in the afternoon it will add 12 before subtraction to get the correct time
+        WorkedHours = parseInt(EndTime.value.slice(0,1)) + 12 - parseInt(StartTime.value.slice(0,2));
+        console.log(WorkedHours);
+    }
+
+    else {// If the end time is not in the afternoon it will just subtract
+        WorkedHours = parseInt(EndTime.value.slice(0,2)) - parseInt(StartTime.value.slice(0,2)); //Adds the Start Time + End Time to get hours worked
+    }
+
     //Formats the date value into dd/mm/yyyy
     const StartMonth = StartDate.value.slice(5,7), //Gets the Month from date value
         StartDay = StartDate.value.slice(8,10), //Gets the Day
